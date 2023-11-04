0985598db02691938972a3e4321f55566da4cc6a
leen-gh <leen.ghaleb.d@gmail.com>
2023-11-05 02:37:16 +0300

    assigment is done

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2
+divide(2000,100)
+const square = (parameter1) => parameter1 * parameter1
+square(10)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
